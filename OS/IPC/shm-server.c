60db7e009c4ebe5bd92fe1c1b68a66e5f73626aa
KarthikSankar <krthknsnkr@gmail.com>
2019-11-06 22:29:42 +0530

    Changed SHM with working code!

--- OS/IPC/shm-server.c
+++ OS/IPC/shm-server.c
@@ -1,29 +1,15 @@
 #include<stdio.h>
 #include<sys/ipc.h>
 #include<sys/shm.h>
-#include<errno.h>
-#define shmsize 27
 #define pf printf
 #define sf scanf
 int main()
 {
-	key_t key;
-	int shmid;
-	char *str;
-	key = 4052;
-	//ftok("shmfile2",65);
-	pf("Key %d\n",key);
-	if(shmid=shmget(key, shmsize, 0666|IPC_CREAT) < 0)
-	{
-		perror("Shared Memory Creation Failed!\n");
-		exit(1);
-	}
-	pf("SHMID %d\n",shmid);
-	str = (char*)shmat(shmid,(void*)0,0);
-	pf("data in shm : %s\n",str);
-	pf("Tell something to other process!\n");
-	gets(str);
-	pf("%s\n",str);
+	key_t key = ftok("shmfile",65);
+	int shmid = shmget(key,27,0666|IPC_CREAT);
+	char *str = (char*)shmat(shmid,NULL,0);
+	pf("Data to be stored in shared memory : ");
+	sf("%s",str);
+	pf("Data stored in shared memory : %s\n", str);
 	shmdt(str);
-	return 0;
 }
