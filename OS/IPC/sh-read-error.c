33ab398c5d907716abd746a476cdd878e3856d80
KarthikSankar <krthknsnkr@gmail.com>
2019-11-08 15:41:43 +0530

    Adding Notes of 8th Oct

---
+++ OS/IPC/sh-read-error.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<sys/ipc.h>
+#include<sys/shm.h>
+#define shmsize 1024
+#define pf printf
+#define sf scanf
+int main()
+{
+	key_t key;
+	int shmid;
+	char* c;
+	key = 5678;
+	if((shmid=shmget(key, shmsize, 0666|IPC_CREAT))<0)
+	{
+		perror("Error creating shared memory!");
+	}
+	c = (char*)shmat(shmid,NULL,0);
+	pf("%s",c);
+	shmdt(c);
+	shmctl(shmid,IPC_RMID,NULL);
+	return 0;
+}
