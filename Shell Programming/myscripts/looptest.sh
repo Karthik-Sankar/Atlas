692288e97266020f7df82088db53f51907a712e0
KarthikSankar <krthknsnkr@gmail.com>
2019-11-22 18:03:28 +0530

    Final Notes for the day!

---
+++ Shell Programming/myscripts/looptest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+arr=(1 2 3 4 5 6 7 8 9 10)
+for i in "${arr[@]}"
+do
+	#v=$(expr $i % 2)
+	if [ $(expr $i % 2) -eq 0 ];
+	then
+		echo "$i is even"
+	else
+		echo "$i is odd"
+	fi
+done
+echo "${arr[@]:0:5}"
+
