a02cef581cae52ceff8aa7cefc98a01662cd346c
karthiksankar <krthknsnkr@gmail.com>
2019-12-06 17:35:01 +0530

    Final Commit Session !

---
+++ Java/Session2/src/OOPSPillars.java
@@ -0,0 +1,14 @@
+/*
+ * 1. Inheritance
+ * 2. Polymorp
+ * 3. Encapsulation
+ * 
+ */
+public class OOPSPillars {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test".contains("es"));
+	}
+
+}
